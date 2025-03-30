960907cd8897a5ffb160ac7527b93d85959ed83c
Alexander Lapsley <74349217+Alex-Lap@users.noreply.github.com>
2025-03-29 20:43:11 -0400

    Update hash.c
    Updated code is about twice as fast as the original

--- hash.c
+++ hash.c
@@ -2,15 +2,30 @@
 #include <string.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <semaphore.h>
+#include <pthread.h>
 
 #include "hash_functions.h"
 
 #define KEEP 16 // only the first 16 bytes of a hash are kept
+#define NUM_WORKERS 3
 
 struct cracked_hash {
 	char hash[2*KEEP+1];
 	char *password, *alg;
+	pthread_mutex_t lock;
 };
+
+typedef struct {
+    char **passwords;
+    int start;
+    int end;
+    struct cracked_hash *cracked_hashes;
+    int n_hashed;
+    pthread_mutex_t *mutex;
+} password_thread_arg_t;
+
+
 
 typedef unsigned char * (*hashing)(unsigned char *, unsigned int);
 
@@ -25,6 +40,38 @@ int compare_hashes(char *a, char *b) {
 	return 1;
 }
 
+
+
+void *crack_thread_func(void *arg) {
+    password_thread_arg_t *targ = (password_thread_arg_t *)arg;
+    char hex_hash[2 * KEEP + 1];
+
+    for (int p = targ->start; p < targ->end; p++) {
+        char *password = targ->passwords[p];
+		char cracked = 0;
+        for (int i = 0; i < n_algs; i++) {
+            unsigned char *hash = fn[i]((unsigned char *)password, strlen(password));
+
+            for (int j = 0; j < KEEP; j++)
+                sprintf(&hex_hash[2 * j], "%02x", hash[j]);
+            hex_hash[2 * KEEP] = '\0';
+
+            for (int j = 0; j < targ->n_hashed; j++) {
+				if (compare_hashes(hex_hash, targ->cracked_hashes[j].hash)) {
+					if (targ->cracked_hashes[j].password == NULL) {
+						targ->cracked_hashes[j].password = strdup(password);
+						targ->cracked_hashes[j].alg = algs[i];
+					}
+
+					break;
+				}
+            }
+        }
+    }
+
+    return NULL;
+}
+
 // Function name: crack_hashed_passwords
 // Description:   Computes different hashes for each password in the password list,
 //                then compare them to the hashed passwords to decide whether if
@@ -34,6 +81,11 @@ void crack_hashed_passwords(char *password_list, char *hashed_list, char *output
 	FILE *fp;
 	char password[256];  // passwords have at most 255 characters
 	char hex_hash[2*KEEP+1]; // hashed passwords have at most 'keep' characters
+
+	pthread_t threads[NUM_WORKERS];//Declare 11 threads
+	sem_t sem; //Declare semaphore
+	sem_init(&sem, 0, 1); //Initialize semaphore
+    int ids[3] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11}; //Declare ids for threads
 
 	// load hashed passwords
 	int n_hashed = 0;
@@ -49,32 +101,51 @@ void crack_hashed_passwords(char *password_list, char *hashed_list, char *output
 		fscanf(fp, "%s", cracked_hashes[i].hash);
 		cracked_hashes[i].password = NULL;
 		cracked_hashes[i].alg = NULL;
+		pthread_mutex_init(&cracked_hashes[i].lock, NULL);
 	}
 	fclose(fp);
 
 	// load common passwords, hash them, and compare them to hashed passwords
 	fp = fopen(password_list, "r");
 	assert(fp != NULL);
-	while(fscanf(fp, "%s", password) == 1) {
+	char **passwords = NULL;
 
-		for(int i=0; i < n_algs; i++) {
-			unsigned char *hash = fn[i]((unsigned char *)password, strlen(password));
-			for(int j=0; j < KEEP; j++)
-				sprintf(&hex_hash[2*j], "%02x", hash[j]);
-			hex_hash[2*KEEP] = '\0';
-			for(int j=0; j < n_hashed; j++) {
-				if(cracked_hashes[j].password !=  NULL)
-					continue;
-				if(compare_hashes(hex_hash, cracked_hashes[j].hash)) {
-					cracked_hashes[j].password = strdup(password);
-					cracked_hashes[j].alg = algs[i];
-					break;
-				}
-			}
-			free(hash);
+	int n_passwords = 0, cap = 1024;
+	passwords = malloc(cap * sizeof(char*));
+	assert(passwords);
+
+	fp = fopen(password_list, "r");
+	assert(fp);
+
+	char buffer[256];
+	while (fscanf(fp, "%s", buffer) == 1) {
+		if (n_passwords == cap) {
+			cap *= 2;
+			passwords = realloc(passwords, cap * sizeof(char*));
 		}
+		passwords[n_passwords++] = strdup(buffer);
 	}
 	fclose(fp);
+
+	password_thread_arg_t args[NUM_WORKERS];
+	pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+	int chunk = (n_passwords + NUM_WORKERS - 1) / NUM_WORKERS;
+
+	for (int i = 0; i < NUM_WORKERS; i++) {
+		args[i].passwords = passwords;
+		args[i].start = i * chunk;
+		args[i].end = (i + 1) * chunk > n_passwords ? n_passwords : (i + 1) * chunk;
+		args[i].cracked_hashes = cracked_hashes;
+		args[i].n_hashed = n_hashed;
+		args[i].mutex = &mutex;
+
+		pthread_create(&threads[i], NULL, crack_thread_func, &args[i]);
+	}
+
+	for (int i = 0; i < NUM_WORKERS; i++) {
+		pthread_join(threads[i], NULL);
+	}
 
 	// print results
 	fp = fopen(output, "w");
